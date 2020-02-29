dad4a4f793432fd409ae507b173e099999579619
Crystal <yxcwuu@gmail.com>
2020-02-29 13:06:36 -0600

    Fix re-routing errors when user creates a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+
+    def new 
+        @post = Post.new
+        @user = User.find(params[:user_id])
+    end
+
+    def create
+        @user = User.find(params[:user_id])
+        @post = @user.posts.create(post_params)
+        if @post.save
+            redirect_to user_post_path(@user, @post.id)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
 end
