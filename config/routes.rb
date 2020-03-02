e14a13f8375e844d4854fb93ae9abe0cd0f46982
Crystal <yxcwuu@gmail.com>
2020-03-02 12:40:41 -0500

    Add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :users do 
     resources :posts 
   end
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
 end
